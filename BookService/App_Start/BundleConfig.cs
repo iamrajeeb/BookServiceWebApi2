60015828563f9dde1fbaee64b46611361d54c9b0
rtamrakar <rajeesa@gmail.com>
2017-05-06 16:51:00 -0500

    knockout implementation added

--- BookService/App_Start/BundleConfig.cs
+++ BookService/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace BookService
 {
@@ -23,6 +22,10 @@ namespace BookService
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            // New code:
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                      "~/Scripts/knockout-{version}.js",
+                      "~/Scripts/app.js"));
         }
     }
 }
