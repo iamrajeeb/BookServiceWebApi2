ee1402d145bb403585af46ef5b82d894c3b209c8
rtamrakar <rajeesa@gmail.com>
2017-05-06 01:43:26 -0500

    First

---
+++ BookService/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookService.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+
+        // Foreign Key
+        public int AuthorId { get; set; }
+        // Virtual Navigation property
+        public virtual Author Author { get; set; }
+    }
+}
